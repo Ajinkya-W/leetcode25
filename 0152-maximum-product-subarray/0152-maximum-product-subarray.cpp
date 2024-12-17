cd998b650fd1bc1e04fb60f21252e40a857a02de
Ajinkya Wasnik <47898069+Ajinkya-W@users.noreply.github.com>
2024-12-18 00:23:02 +0530

    Update 0152-maximum-product-subarray.cpp

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,6 +1,19 @@
 class Solution {
 private:
-    int maxProductHelper(vector<int>& nums){
+    int maxProductHelper1(vector<int>& nums){
+        int prod=1, maxProd=INT_MIN;
+        for(int i=0; i< nums.size(); i++){
+            prod = (prod == 0) ? nums[i] : prod * nums[i];
+            maxProd = max(prod,maxProd);
+        }
+        prod = 1;
+        for(int i=nums.size()-1; i>=0; i--){
+            prod = (prod == 0) ? nums[i] : prod * nums[i];
+            maxProd = max(prod,maxProd);
+        }
+        return maxProd;
+    }
+    int maxProductHelper2(vector<int>& nums){
         int prod=1, maxProd=INT_MIN;
         for(int i=0; i< nums.size(); i++){
             prod = (prod == 0) ? nums[i] : prod * nums[i];
