571379cc022629fac998794837f2363cd4f8a545
Ajinkya Wasnik <47898069+Ajinkya-W@users.noreply.github.com>
2024-12-18 00:18:10 +0530

    Time: 0 ms (100%), Space: 17.6 MB (26.44%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -3,19 +3,13 @@ private:
     int maxProductHelper(vector<int>& nums){
         int prod=nums[0], maxProd=max(INT_MIN,nums[0]), maxNum =nums[0];
         for(int i=1; i< nums.size(); i++){
-            if(prod == 0)
-                prod = nums[i];
-            else
-                prod*= nums[i]; 
+            prod = (prod == 0) ? nums[i] : prod * nums[i];
             maxProd = max(prod,maxProd);
             maxNum = max(maxNum, nums[i]);
         }
         prod = nums[nums.size()-1];
         for(int i=nums.size()-2; i>=0; i--){
-            if(prod == 0)
-                prod = nums[i]; 
-            else
-                prod*= nums[i]; 
+            prod = (prod == 0) ? nums[i] : prod * nums[i];
             maxProd = max(prod,maxProd);
         }
         
