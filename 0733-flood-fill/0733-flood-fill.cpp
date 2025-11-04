2cf686b6368e18b74c1e4a0d067a3c573262d823
Ajinkya Wasnik <47898069+Ajinkya-W@users.noreply.github.com>
2025-11-04 12:33:45 +0530

    Time: 0 ms (100%), Space: 18.1 MB (57.28%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,25 +1,26 @@
 class Solution {
+    const int dcol[4]={0,1,-1,0};
+    const int drow[4]={1,0, 0,-1};
 private:
-    void dfsHelper(vector<vector<int>> &grid, vector<vector<bool>> &visited, int r, int c, int numRows, int numCols, int originalColor, int color){
-        visited[r][c] = 1;
-        int dcol[4]={0,1,-1,0};
-        int drow[4]={1,0, 0,-1};
+    void dfsHelper(vector<vector<int>> &grid, int r, int c, int numRows, int numCols, int originalColor, int color){
+        grid[r][c] = color;
         for(int i=0; i < 4; i++){
             int newr = drow[i]+r;
             int newc = dcol[i]+c;
-            if(newr<numRows && newr>=0 && newc>=0 && newc<numCols && !visited[newr][newc] && grid[newr][newc]==originalColor){
-                grid[newr][newc] = color;
-                dfsHelper(grid, visited, newr, newc, numRows, numCols, originalColor, color);
+            if(newr<numRows && newr>=0 && newc>=0 && newc<numCols && grid[newr][newc]==originalColor){
+                dfsHelper(grid, newr, newc, numRows, numCols, originalColor, color);
             }
         }
     }
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& grid, int sr, int sc, int color) {
         const int numRows=grid.size(), numCols=grid[0].size();
-        vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size(),0));
         int originalColor = grid[sr][sc];
-        dfsHelper(grid, visited, sr, sc, numRows, numCols, originalColor, color);
-        grid[sr][sc] = color;
+        if (originalColor == color) {
+            return grid;
+        }
+
+        dfsHelper(grid, sr, sc, numRows, numCols, originalColor, color);
         return grid;
     }
 };
