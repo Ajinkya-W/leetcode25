e22e1489cce90cd3ca72fd3789dbe8f18dd38de8
Ajinkya Wasnik <47898069+Ajinkya-W@users.noreply.github.com>
2025-11-23 15:49:39 +0530

    Time: 0 ms (100%), Space: 18.3 MB (32.47%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,26 +1,22 @@
 class Solution {
-    const int dcol[4]={0,1,-1,0};
-    const int drow[4]={1,0, 0,-1};
 private:
-    void dfsHelper(vector<vector<int>> &grid, int r, int c, int numRows, int numCols, int originalColor, int color){
-        grid[r][c] = color;
-        for(int i=0; i < 4; i++){
-            int newr = drow[i]+r;
-            int newc = dcol[i]+c;
-            if(newr<numRows && newr>=0 && newc>=0 && newc<numCols && grid[newr][newc]==originalColor){
-                dfsHelper(grid, newr, newc, numRows, numCols, originalColor, color);
+    void dfsColourFill(vector<vector<int>>& image, vector<vector<bool>>& visited, int sr, int sc, int color, int ogColor){
+        if((sr>=0 && sr<image.size()) && (sc>=0 && sc<image[0].size()) && visited[sr][sc]==0){
+            visited[sr][sc] = 1;
+            if(image[sr][sc]==ogColor){
+                image[sr][sc] = color;
+                dfsColourFill(image, visited, sr+1, sc, color, ogColor);
+                dfsColourFill(image, visited, sr-1, sc, color, ogColor);
+                dfsColourFill(image, visited, sr, sc-1, color, ogColor);
+                dfsColourFill(image, visited, sr, sc+1, color, ogColor);
             }
         }
     }
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& grid, int sr, int sc, int color) {
-        const int numRows=grid.size(), numCols=grid[0].size();
-        int originalColor = grid[sr][sc];
-        if (originalColor == color) {
-            return grid;
-        }
-
-        dfsHelper(grid, sr, sc, numRows, numCols, originalColor, color);
-        return grid;
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        vector<vector<bool>> visited(image.size(), vector<bool> (image[0].size(), 0));
+        int ogColor = image[sr][sc];
+        dfsColourFill(image, visited, sr, sc, color, ogColor);
+        return image;
     }
 };
