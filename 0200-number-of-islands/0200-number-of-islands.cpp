c1c349a4618467a23fb584f010c03c1869a572b6
Ajinkya Wasnik <47898069+Ajinkya-W@users.noreply.github.com>
2025-11-23 15:25:28 +0530

    Time: 25 ms (71.74%), Space: 16.7 MB (59.6%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,29 +1,26 @@
 class Solution {
 private:
-    void dfsHelper(const vector<vector<char>> &grid, vector<vector<bool>> &visited, int r, int c, int numRows, int numCols){
-        visited[r][c] = 1;
-        int dcol[4]={0,1,-1,0};
-        int drow[4]={1,0, 0,-1};
-        for(int i=0; i < 4; i++){
-            int newr = drow[i]+r;
-            int newc = dcol[i]+c;
-            if(newr<numRows && newr>=0 && newc>=0 && newc<numCols && grid[newr][newc]=='1' && !visited[newr][newc])
-                dfsHelper(grid, visited, newr, newc, numRows, numCols);
+    void dfsHelper(vector<vector<char>>& grid, vector<vector<bool>>& visited, int rows, int cols, int i, int j){
+        if((i < rows && i>=0) && (j < cols && j >= 0) && !visited[i][j] && grid[i][j]=='1'){
+            visited[i][j] = 1;
+            dfsHelper(grid, visited, rows, cols, i+1, j);
+            dfsHelper(grid, visited, rows, cols, i, j+1);
+            dfsHelper(grid, visited, rows, cols, i, j-1);
+            dfsHelper(grid, visited, rows, cols, i-1, j);
         }
     }
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int islands =0;
-        const int numRows=grid.size(), numCols=grid[0].size();
-        vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size(),0));
-        for(int r=0; r<grid.size(); r++){
-            for(int c=0; c<grid[0].size(); c++){
-                if(!visited[r][c] && grid[r][c]=='1'){
-                    islands++;
-                    dfsHelper(grid, visited, r, c, numRows, numCols);
+        int rows = grid.size(), cols = grid[0].size(), numberOfIsland = 0;
+        vector<vector<bool>> visited(rows, vector<bool> (cols, 0));
+        for(int i = 0; i < rows; i++){
+            for(int  j = 0; j < cols; j++){
+                if(!visited[i][j] && grid[i][j]=='1'){
+                    numberOfIsland++;
+                    dfsHelper(grid, visited, rows, cols, i, j);
                 }
             }
         }
-        return islands;
+        return numberOfIsland;
     }
 };
