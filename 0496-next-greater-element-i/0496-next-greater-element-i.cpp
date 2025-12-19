9bd6f2de9b4da1d8018913875739a7efad187557
Ajinkya Wasnik <47898069+Ajinkya-W@users.noreply.github.com>
2025-12-19 08:28:43 +0530

    Time: 3 ms (40.78%), Space: 12.9 MB (35.61%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,8 +1,4 @@
 class Solution {
-private:
-    bool elementExistsInHMap(int num, unordered_map<int, int>& nums1HMap){
-        return nums1HMap.find(num)!=nums1HMap.end();
-    }
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         unordered_map<int, int> nums1HMap;
@@ -12,19 +8,11 @@ public:
         }
         stack<int> nextGreaterEleStk;
         for(int currNum: nums2){
-            int stkSize = nextGreaterEleStk.size();
-            if(stkSize>0){
-                int topNum = nextGreaterEleStk.top();
-                while(topNum < currNum){
-                    if(elementExistsInHMap(topNum, nums1HMap)){
-                        nums1HMap[topNum] = currNum;
-                    }
-                    nextGreaterEleStk.pop();
-                    if(nextGreaterEleStk.size()==0) 
-                        break;
-                    else
-                        topNum = nextGreaterEleStk.top();
-                }   
+            while(nextGreaterEleStk.size()>0 && nextGreaterEleStk.top() < currNum){
+                if(nums1HMap.find(nextGreaterEleStk.top())!=nums1HMap.end()){
+                    nums1HMap[nextGreaterEleStk.top()] = currNum;
+                }
+                nextGreaterEleStk.pop();  
             } 
             nextGreaterEleStk.push(currNum);
         }
