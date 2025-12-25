fa21a1ef36fa4a8357d24d159bec4ef14e2e6018
Ajinkya Wasnik <47898069+Ajinkya-W@users.noreply.github.com>
2025-12-25 11:06:29 +0530

    Time: 0 ms (100%), Space: 26 MB (58.09%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,51 +2,21 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        if(n == 0) return 0;
-        int left = 0, right = n - 1;
-        int leftMax = 0, rightMax = 0, volume = 0;
-        while (left <= right) {
-            if(height[left] < height[right]){
-                if(height[left] > leftMax) // as vol update be 0 in this case
-                    leftMax = height[left];
-                else
-                    volume += leftMax - height[left];
-                left++;
+        
+        int left = 0, right = n-1, leftBoundary = 0, rightBoundary = 0;
+        int totalWater = 0;
+
+        while(left <= right){
+            if(leftBoundary <= rightBoundary){
+                leftBoundary = max(leftBoundary, height[left]);
+                
+                totalWater += leftBoundary - height[left++];
             } else{
-                if(height[right] > rightMax)
-                    rightMax = height[right];
-                else
-                    volume += rightMax - height[right];
-                right--;
-            } 
+                rightBoundary = max(rightBoundary, height[right]);
+                
+                totalWater += rightBoundary - height[right--];
+            }   
         }
-        return volume;
+        return totalWater;
     }
 };
-// class Solution {
-// public:
-//     int trap(vector<int>& height) {
-//         int n = height.size();
-//         if(n == 0) return 0;
-//         int left = 0, right = n - 1;
-//         int leftMax = 0, rightMax = 0, volume = 0;
-//         while (left <= right) {
-//             if(height[left] > leftMax){
-//                 leftMax = height[left];
-//             }
-//             if(height[right] > rightMax){
-//                 rightMax = height[right];
-//             }
-
-//             if(leftMax<rightMax){
-//                 volume += leftMax - height[left];
-//                 left++;
-//             }
-//             else{
-//                 volume += rightMax - height[right];
-//                 right--;
-//             }
-//         }
-//         return volume;
-//     }
-// };
