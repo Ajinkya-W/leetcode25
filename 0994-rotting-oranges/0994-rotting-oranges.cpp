c2b014a3498c8ee679b6070b0ff039cb8c60be37
Ajinkya Wasnik <47898069+Ajinkya-W@users.noreply.github.com>
2025-12-26 16:08:25 +0530

    Time: 4 ms (17.36%), Space: 16.7 MB (87.41%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,48 +1,47 @@
 class Solution {
-private:
-    int rottingBfs(vector<vector<int>>& grid, int& freshOranges, queue<pair<int, int>>& q){
-        int timeElapsed = 0;
-        int dr[4] = {0, -1, 0, 1}, dc[4] = {1, 0, -1, 0};
-        while(!q.empty()){
-            bool didRottingHappen = 0;
-            int qSize = q.size();
-            for(int i=0; i < qSize; i++){
-                int row = q.front().first;
-                int col = q.front().second;
-                for(int i=0; i<4; i++){
-                    int newRow = row + dr[i];
-                    int newCol = col + dc[i];
-                    if((newRow>=0 && newRow<grid.size()) && (newCol>=0 && newCol<grid[0].size()) && grid[newRow][newCol]==1){
-                        grid[newRow][newCol] = 2;
-                        q.push({newRow, newCol});
-                        freshOranges--;
-                        didRottingHappen = true;
-                    }
-                } 
-                q.pop();
-            }
-            if(didRottingHappen){
-                timeElapsed++;
-            }
-        }
-        return timeElapsed;
-    }
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+        int row = grid.size(), col = grid[0].size();
+        int minutes = 0, freshOranges = 0;
         queue<pair<int, int>> q;
-        int totalTimeElapsed = 0, freshOranges = 0;
-        for(int row = 0; row < grid.size(); row++){
-            for(int col = 0; col < grid[0].size(); col++){
-                if(grid[row][col]==2){
-                    q.push({row, col});
-                } else if(grid[row][col]==1){
+
+        // precompute
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(grid[i][j] == 2){
+                    q.push({i, j});
+                } else if(grid[i][j] == 1){
                     freshOranges++;
                 }
             }
         }
-        totalTimeElapsed = rottingBfs(grid, freshOranges, q);
-        if(freshOranges>0)
-            return -1;
-        return totalTimeElapsed;
+        if(freshOranges==0) return 0;
+        
+        // rot
+        while(!q.empty()){
+            int qSize = q.size();
+            
+            for(int i = 0; i < qSize; i++){
+                int r = q.front().first, c = q.front().second;
+                
+                if(r-1>=0 && grid[r-1][c] == 1){
+                    grid[r-1][c] = 2; q.push({r-1, c}); freshOranges--;
+                }
+                if(r+1<row && grid[r+1][c] == 1){
+                    grid[r+1][c] = 2; q.push({r+1, c}); freshOranges--;
+                }
+                if(c-1>=0 && grid[r][c-1] == 1){
+                    grid[r][c-1] = 2; q.push({r, c-1}); freshOranges--;
+                }
+                if(c+1<col && grid[r][c+1] == 1){
+                    grid[r][c+1] = 2; q.push({r, c+1}); freshOranges--;
+                }
+                q.pop();
+            }
+            if(!q.empty())
+                minutes++;
+        }
+        
+        return freshOranges > 0? -1: minutes;
     }
 };
