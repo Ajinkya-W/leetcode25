14cbc63503b37daf3d4114a75c6ded52847889d1
Ajinkya Wasnik <47898069+Ajinkya-W@users.noreply.github.com>
2025-12-27 12:01:33 +0530

    Time: 11 ms (46.26%), Space: 27.4 MB (99.05%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -2,22 +2,18 @@ class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int r = matrix.size(), c = matrix[0].size();
-        vector<int> dp(c, 0);
+        vector<int> dp(c+1, 0);
         int maxSide = 0;
-        
-        for(int i = 0; i < r; i++){
+
+        for(int i = 1; i <= r; i++){
             int diag = 0;
-            for(int j = 0; j < c; j++){
+            for(int j = 1; j <= c; j++){
                 int currUp = dp[j];
-                if(matrix[i][j]=='0'){
-                    dp[j] = 0;
-                } else{
-                    if(i==0 || j==0){
-                        dp[j] = 1;
-                    } else{
-                        dp[j] = 1 + min({dp[j], dp[j-1], diag});
-                    }
-                }
+                
+                if(matrix[i-1][j-1]=='1')
+                    dp[j] = 1 + min({dp[j], dp[j-1], diag});
+                else dp[j] = 0;
+
                 maxSide = max(maxSide, dp[j]);
                 diag = currUp;
             }
