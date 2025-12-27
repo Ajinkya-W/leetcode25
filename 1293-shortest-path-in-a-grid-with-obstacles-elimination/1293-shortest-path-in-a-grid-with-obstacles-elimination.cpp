5ee7613a6489d9dd28a1d3ad6ec24b97245f0e82
Ajinkya Wasnik <47898069+Ajinkya-W@users.noreply.github.com>
2025-12-27 12:17:16 +0530

    Time: 27 ms (70.26%), Space: 20.1 MB (69.37%) - LeetHub

---
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int shortestPath(vector<vector<int>>& grid, int k) {
+        int row = grid.size();
+        int col = grid[0].size();
+        int freshOranges = 0;
+
+        queue<vector<int>> q;
+        vector<vector<int>> maxK(row, vector<int>(col, -1));
+
+        // 1. Initial scan: Only one source (0,0)
+        q.push({0, 0, k}); 
+        maxK[0][0] = k;
+    
+        const int dr[4] = {0, 1, -1, 0};
+        const int dc[4] = {1, 0, 0, -1};
+        int steps = 0;
+
+        // 2. BFS
+        while(!q.empty()){
+            int qSize = q.size(); 
+            
+            for(int i = 0; i < qSize; i++){
+                int r = q.front()[0];
+                int c = q.front()[1];
+                int curK = q.front()[2]; // CHANGE: track remaining k
+                q.pop();
+
+                if(r == row - 1 && c == col - 1) return steps;
+
+                for(int j = 0; j < 4; j++){
+                    int R = r + dr[j];
+                    int C = c + dc[j];
+                    
+                    // Boundary check and validation: Only "infect" fresh oranges.
+                    if(R >= 0 && R < row && C >= 0 && C < col){
+                        // CHANGE: Calculate new K based on if next cell is a wall
+                        int nextK = curK - grid[R][C];
+
+                        // CHANGE: Only proceed if K is valid AND state is "stronger"
+                        if(nextK >= 0 && nextK > maxK[R][C]){
+                            maxK[R][C] = nextK; // Mark as visited with this strength
+                            q.push({R, C, nextK});
+                        }
+                    }
+                }
+            }
+            steps++;
+        }
+        return -1;
+    }
+};
