2f4302bbcd4b51787dd248e31ad53c69ec28cf2e
Ajinkya Wasnik <47898069+Ajinkya-W@users.noreply.github.com>
2026-01-03 08:50:10 +0530

    Time: 0 ms (100%), Space: 34.3 MB (72.12%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -11,7 +11,7 @@
  */
 class Solution {
 public:
-    TreeNode* deleteNode(TreeNode* root, int key) {
+    TreeNode* deleteNode1(TreeNode* root, int key) {
         if (!root) return nullptr;
 
         if (key < root->val) {
@@ -32,6 +32,50 @@ public:
             
             root->val = succ->val;
             root->right = deleteNode(root->right, succ->val);
+        }
+        return root;
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (!root) return nullptr;
+
+        if (key < root->val) {
+            root->left = deleteNode(root->left, key);
+        } else if (key > root->val) {
+            root->right = deleteNode(root->right, key);
+        } else {
+            // Cases 0 & 1: No child or 1 child
+            if (!root->left) {
+                TreeNode* temp = root->right;
+                delete root;
+                return temp;
+            }
+            if (!root->right) {
+                TreeNode* temp = root->left;
+                delete root;
+                return temp;
+            }
+
+            // Case 2: Two children (Physically Move Successor)
+            // 1. Find the parent of the successor
+            TreeNode* succParent = root;
+            TreeNode* succ = root->right;
+            while (succ->left) {
+                succParent = succ;
+                succ = succ->left;
+            }
+
+            // 2. Disconnect successor from its original parent
+            if (succParent != root) {
+                succParent->left = succ->right;
+                succ->right = root->right; // Successor takes over old right subtree
+            }
+
+            // 3. Connect successor to old left subtree
+            succ->left = root->left;
+
+            // 4. Delete old root and return the successor as the new root
+            delete root;
+            return succ;
         }
         return root;
     }
