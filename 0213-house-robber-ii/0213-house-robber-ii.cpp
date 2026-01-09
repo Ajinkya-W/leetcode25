b44621893ab7513417e3a01177040cc1f6dfe90d
Ajinkya Wasnik <47898069+Ajinkya-W@users.noreply.github.com>
2026-01-09 10:43:03 +0530

    Time: 0 ms (100%), Space: 10.3 MB (73.9%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 1)
-            return nums[0];
-
-        return max(robNormal(nums, 0, n - 1), robNormal(nums, 1, n));
-    }
+    // This is your core "Decision" engine
     int robNormal(vector<int>& nums, int start, int end) {
+        int prev = 0;       // Best 1 step ago
+        int beforePrev = 0; // Best 2 steps ago
 
-        if (0 == end - start)
-            return nums[start];
-
-        int prev = nums[start], beforePrev = 0;
-
-        for (int i = start + 1; i < end; i++) {
+        for (int i = start; i <= end; i++) {
             int curr = max(nums[i] + beforePrev, prev);
             beforePrev = prev;
             prev = curr;
         }
         return prev;
+    }
+
+    // HR1 (Linear)
+    int rob1(vector<int>& nums) {
+        return robNormal(nums, 0, nums.size() - 1);
+    }
+
+    // HR2 (Circular)
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return nums[0]; // Essential because ranges would overlap
+        
+        return max(robNormal(nums, 0, n - 2), 
+                robNormal(nums, 1, n - 1));
     }
 };
