4fbbd0cedb7fd4150ffe8f00d0444529c7659029
Ajinkya Wasnik <47898069+Ajinkya-W@users.noreply.github.com>
2026-01-09 13:25:57 +0530

    Time: 24 ms (77.22%), Space: 16.4 MB (65.42%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,26 +1,28 @@
 class Solution {
 private:
-    void dfsHelper(vector<vector<char>>& grid, vector<vector<bool>>& visited, int rows, int cols, int i, int j){
-        if((i < rows && i>=0) && (j < cols && j >= 0) && !visited[i][j] && grid[i][j]=='1'){
-            visited[i][j] = 1;
-            dfsHelper(grid, visited, rows, cols, i+1, j);
-            dfsHelper(grid, visited, rows, cols, i, j+1);
-            dfsHelper(grid, visited, rows, cols, i, j-1);
-            dfsHelper(grid, visited, rows, cols, i-1, j);
+    void dfsIslands(vector<vector<char>>& grid, int i, int j, vector<vector<bool>> &visited){
+        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == '0' || visited[i][j]){
+            return;
         }
+        visited[i][j] = 1;
+        
+        dfsIslands(grid, i-1, j, visited);
+        dfsIslands(grid, i+1, j, visited);
+        dfsIslands(grid, i, j-1, visited);
+        dfsIslands(grid, i, j+1, visited);
     }
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int rows = grid.size(), cols = grid[0].size(), numberOfIsland = 0;
-        vector<vector<bool>> visited(rows, vector<bool> (cols, 0));
-        for(int i = 0; i < rows; i++){
-            for(int  j = 0; j < cols; j++){
-                if(!visited[i][j] && grid[i][j]=='1'){
-                    numberOfIsland++;
-                    dfsHelper(grid, visited, rows, cols, i, j);
+        int islands = 0, row = grid.size(), col = grid[0].size();
+        vector<vector<bool>> visited(row, vector<bool>(col, false));
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(!visited[i][j] && grid[i][j] == '1'){
+                    islands++;
+                    dfsIslands(grid, i, j, visited);
                 }
             }
         }
-        return numberOfIsland;
+        return islands;
     }
 };
