4f23a951c973e9650f337d24ae52d3f299ce7dc5
Ajinkya Wasnik <47898069+Ajinkya-W@users.noreply.github.com>
2026-01-09 12:25:34 +0530

    Time: 0 ms (100%), Space: 15.5 MB (80.73%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -3,21 +3,38 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int row = grid.size(), col = grid[0].size();
 
-        vector<vector<int>> dp(row, vector<int> (col, 0));
-        dp[0][0] = grid[0][0];
+        vector<int> dp(col, 0);
+        dp[0] = grid[0][0];
         // first row initialisation
-        for(int j = 1; j < col; j++){
-            dp[0][j] = grid[0][j] + dp[0][j-1];
-        }
-        // first col initialisation
+        for(int j = 1; j < col; j++)
+            dp[j] = grid[0][j] + dp[j-1];
+        
         for(int i = 1; i < row; i++){
-            dp[i][0] = grid[i][0] + dp[i-1][0];
-        }
-        for(int i = 1; i < row; i++){
+            dp[0] += grid[i][0]; // first col so add above values
             for(int j = 1; j < col; j++){
-                dp[i][j] = grid[i][j] + min(dp[i][j-1], dp[i-1][j]);
+                dp[j] = grid[i][j] + min(dp[j-1], dp[j]);
             }
         }
-        return dp[row-1][col-1];
+        return dp[col-1];
     }
+    // int minPathSum2D(vector<vector<int>>& grid) {
+    //     int row = grid.size(), col = grid[0].size();
+
+    //     vector<vector<int>> dp(row, vector<int> (col, 0));
+    //     dp[0][0] = grid[0][0];
+    //     // first row initialisation
+    //     for(int j = 1; j < col; j++){
+    //         dp[0][j] = grid[0][j] + dp[0][j-1];
+    //     }
+    //     // first col initialisation
+    //     for(int i = 1; i < row; i++){
+    //         dp[i][0] = grid[i][0] + dp[i-1][0];
+    //     }
+    //     for(int i = 1; i < row; i++){
+    //         for(int j = 1; j < col; j++){
+    //             dp[i][j] = grid[i][j] + min(dp[i][j-1], dp[i-1][j]);
+    //         }
+    //     }
+    //     return dp[row-1][col-1];
+    // }
 };
