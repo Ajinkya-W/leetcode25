30e58353962267bae623cac1054c253efcd64f99
Ajinkya Wasnik <47898069+Ajinkya-W@users.noreply.github.com>
2026-01-10 15:44:25 +0530

    Time: 35 ms (56.93%), Space: 39.9 MB (79.23%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -1,22 +1,22 @@
-class DisjointSet{ 
+class DisjointSet {
 public:
     vector<int> parent;
-    DisjointSet(int n){
+    DisjointSet(int n) {
         parent.resize(n);
-        for (int i = 0; i < n; i++) 
+        for (int i = 0; i < n; i++)
             parent[i] = i;
     }
-    int find(int x){
-        if(parent[x] == x){
+    int find(int x) {
+        if (parent[x] == x) {
             return x;
         }
         return parent[x] = find(parent[x]);
     }
-    void unite(int x, int y){
-        int xParent = find(x);
-        int yParent = find(y);
-        if(xParent != yParent){
-            parent[xParent] = yParent;
+    void unite(int x, int y) {
+        int xRoot = find(x);
+        int yRoot = find(y);
+        if (xRoot != yRoot) {
+            parent[xRoot] = yRoot;
         }
     }
 };
@@ -29,43 +29,47 @@ public:
         DisjointSet ds(n);
         unordered_map<string, int> emailMap; // email to index map
 
+        // Phase 1: Build the connectivity graph using account indices    
         for (int i = 0; i < n; i++) {
-            for (int j = 1; j < accounts[i].size(); j++) {
-
-                auto itr = emailMap.find(accounts[i][j]);
+            const auto& account = accounts[i];
+            for (int j = 1; j < account.size(); j++) {
+                const string& email = account[j];
+                auto itr = emailMap.find(email);
                 if (itr == emailMap.end()) {
-                    emailMap[accounts[i][j]] = i;
-                } else{
-                    ds.unite(i, emailMap[accounts[i][j]]);
+                    emailMap[email] = i;
+                } else {
+                    // Email seen before: Bridge found. 
+                    // Merge current account index with the previously mapped account index.
+                    ds.unite(i, itr->second);
                 }
             }
         }
-        // traverse through the ds and see parent, childs
-        // that way we knw, which indexes are parent and all the ones who are childs map to same parent index
-        // make parent to child map so it becomes easy to get result
-        unordered_map<int, vector<int>> parentChildMap;
-        for(int child = 0; child < ds.parent.size(); child++){
-            int parent = ds.find(child);
-            parentChildMap[parent].push_back(child);
+        // Phase 2: Group account indices by their absolute representative (root)
+
+        unordered_map<int, vector<int>> rootChildMap;
+        for (int child = 0; child < ds.parent.size(); child++) {
+            int root = ds.find(child);
+            rootChildMap[root].push_back(child);
         }
 
         vector<vector<string>> resultAccounts;
-        for(auto index: parentChildMap){
-            int parent = index.first;
-            vector<int> children = index.second;
-            set<string> childParentEmails;
-            for(int i = 1; i < accounts[parent].size(); i++){
-                childParentEmails.insert(accounts[parent][i]);
+
+        for (const auto& [root, children] : rootChildMap){
+
+            vector<string> currentComponentEmails;
+
+            for (const auto& child : children){
+                const vector<string>& emails = accounts[child];
+                currentComponentEmails.insert(currentComponentEmails.end(), emails.begin()+1, emails.end());
             }
-            for(const auto &child: children)
-                for(int i = 1; i < accounts[child].size(); i++){
-                    childParentEmails.insert(accounts[child][i]);
-                }
+            sort(currentComponentEmails.begin(), currentComponentEmails.end());
+            currentComponentEmails.erase(unique(currentComponentEmails.begin(), currentComponentEmails.end()), currentComponentEmails.end());
+
             vector<string> emailRow;
-            emailRow.push_back(accounts[parent][0]);
-            for(auto email: childParentEmails){
-                emailRow.push_back(email);
-            }
+
+            emailRow.push_back(accounts[root][0]);
+            emailRow.insert(emailRow.end(), currentComponentEmails.begin(), currentComponentEmails.end());
+            
             resultAccounts.push_back(emailRow);
         }
         return resultAccounts;
