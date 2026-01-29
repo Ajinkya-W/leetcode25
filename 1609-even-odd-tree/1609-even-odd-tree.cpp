fb3cde3d88265693e4d4fc3536dbabdce735459f
Ajinkya Wasnik <47898069+Ajinkya-W@users.noreply.github.com>
2026-01-29 15:40:28 +0530

    Time: 7 ms (51.22%), Space: 153.8 MB (60.63%) - LeetHub

--- 1609-even-odd-tree/1609-even-odd-tree.cpp
+++ 1609-even-odd-tree/1609-even-odd-tree.cpp
@@ -13,57 +13,44 @@
 class Solution {
 public:
     bool isEvenOddTree(TreeNode* root) {
+        if (!root)
+            return true;
+
         queue<TreeNode*> q;
         q.push(root);
         int level = 0;
-        
+
         while (!q.empty()) {
             int qSize = q.size();
-            if(root->val%2==0) return false;
-            level++;
-            if (level % 2 == 0) {
-                int prev = INT_MIN;
-                for (int i = 0; i < qSize; i++) {
-                    TreeNode* front = q.front();
-                    q.pop();
-                    TreeNode *left = front->left, *right = front->right;
-                    if (left) {
-                        if (left->val > prev && left->val % 2 != 0) {
-                            q.push(left);
-                            prev = left->val;
-                        } else
-                            return false;
-                    }
-                    if (right) {
-                        if (right->val > prev && right->val % 2 != 0) {
-                            q.push(right);
-                            prev = right->val;
-                        } else
-                            return false;
-                    }
+
+            int prev = level % 2 == 0 ? INT_MIN : INT_MAX;
+
+            bool isEvenLevel = (level % 2 == 0);
+            
+            for (int i = 0; i < qSize; i++) {
+                TreeNode* front = q.front();
+                q.pop();
+
+                int currVal = front->val;
+
+                if (isEvenLevel) {
+                    if (currVal % 2 != 0 && currVal > prev) {
+                        prev = currVal;
+                    } else
+                        return false;
+                } else {
+                    if (currVal % 2 == 0 && currVal < prev)
+                        prev = currVal;
+                    else
+                        return false;
                 }
-            } else {
-                int prev = INT_MAX;
-                for (int i = 0; i < qSize; i++) {
-                    TreeNode* front = q.front();
-                    q.pop();
-                    TreeNode *left = front->left, *right = front->right;
-                    if (left) {
-                        if (left->val < prev && left->val % 2 == 0) {
-                            q.push(left);
-                            prev = left->val;
-                        } else
-                            return false;
-                    }
-                    if (right) {
-                        if (right->val < prev && right->val % 2 == 0) {
-                            q.push(right);
-                            prev = right->val;
-                        } else
-                            return false;
-                    }
-                }
+
+                if (front->left)
+                    q.push(front->left);
+                if (front->right)
+                    q.push(front->right);
             }
+            level++;
         }
         return true;
     }
