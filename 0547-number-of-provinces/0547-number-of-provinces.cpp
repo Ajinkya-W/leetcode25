cb6656fca855f5ad1eb02b4d8b2a6b0e48699bd1
Ajinkya Wasnik <47898069+Ajinkya-W@users.noreply.github.com>
2026-02-10 01:30:07 +0530

    Time: 1 ms (48.65%), Space: 19.6 MB (64.12%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,6 +1,6 @@
 class Solution {
 private:
-    void bfs(vector<vector<int>>& adj, vector<bool>& visited, int curr){
+    void bfs(vector<vector<int>>& matrix, vector<bool>& visited, int curr){
         visited[curr] = 1;
 
         queue<int> q;
@@ -10,8 +10,8 @@ private:
             int u = q.front();
             q.pop();
 
-            for(int &v: adj[u]){
-                if(!visited[v]){
+            for(int v = 0; v < matrix[u].size(); v++){
+                if(!visited[v] && matrix[u][v] == 1){
                     q.push(v);
                     visited[v] = 1;
                 }
@@ -22,21 +22,13 @@ private:
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int V = isConnected.size();
-        vector<vector<int>> adj(V);
-        for(int i = 0; i < V; i++){ // make V/2
-            for(int j = 0; j < V; j++){
-                if(isConnected[i][j] == 1 && i!=j){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
-                }
-            }
-        }
+        
         int provinces = 0;
         vector<bool> visited(V, 0);
 
         for(int i = 0; i < V; i++){
             if(!visited[i]){
-                bfs(adj, visited, i);
+                bfs(isConnected, visited, i);
                 provinces++;
             }
         }
