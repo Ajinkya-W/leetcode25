13d272c642677141912749c7e9111b4c8d318456
Ajinkya Wasnik <47898069+Ajinkya-W@users.noreply.github.com>
2026-02-10 10:23:43 +0530

    Time: 0 ms (100%), Space: 17.7 MB (24.8%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& adj, vector<int>& color, int curr, int currColor){
+    bool bfs(vector<vector<int>>& adj, vector<int> &color, int curr, int currColor){
+        queue<int> q;
+        q.push(curr);
         color[curr] = currColor;
 
-        for(int &v: adj[curr]){
-            if(color[v] == color[curr])
-                return false;
-            if(color[v] == -1 && !dfs(adj, color, v, 1 - currColor)){
-                return false;
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+
+            for(int &v: adj[u]){
+                if(color[v] == color[u])
+                    return false;
+                else if(color[v] == -1){
+                    q.push(v);
+                    color[v] = 1 - color[u];
+                }
             }
         }
         return true;
@@ -17,7 +25,7 @@ public:
         vector<int> color(V, -1);
 
         for(int i = 0; i < V; i++){
-            if(color[i] == -1 && !dfs(adj, color, i, 0)){
+            if(color[i] == -1 && !bfs(adj, color, i, 0)){
                 return false;
             }
         }
