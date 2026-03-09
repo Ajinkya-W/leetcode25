7de27251c55f07ff14313072086f604955e9d611
Ajinkya Wasnik <47898069+Ajinkya-W@users.noreply.github.com>
2026-03-09 16:50:19 +0530

    Time: 0 ms (100%), Space: 11.2 MB (62.26%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.cpp
+++ 1854-maximum-population-year/1854-maximum-population-year.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
     int maximumPopulation(vector<vector<int>>& logs) {
-        vector<int> delta(102, 0);
-        for(auto& log: logs){
-            int start = log[0], end = log[1];
-            delta[start - 1950]++;
-            delta[end - 1950]--;
+        int maxYear;
+        int maxPopulation = 0;
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq; // {birthYear, deathYear}
+
+        for(int i = 0; i < logs.size(); i++){
+            int bornYear = logs[i][0];
+            int deathYear = logs[i][1];
+
+            pq.push(make_pair(bornYear, 1));
+            pq.push(make_pair(deathYear, -1));
         }
-        int peopleAliveNow = 0, maxAlivePopulation = 0, bestYearToLive;
-        for(int i = 0; i < delta.size(); i++){
-            int val = delta[i];
-            peopleAliveNow += val;
-            if(maxAlivePopulation < peopleAliveNow){
-                maxAlivePopulation = peopleAliveNow;
-                bestYearToLive = i + 1950;
+        int population = 0;
+        while(!pq.empty()){
+            bool isDeathYear = pq.top().second == -1? 1: 0;
+            int currYear = pq.top().first;
+            pq.pop();
+            if(isDeathYear){
+                population--;
+            } else{
+                population++;
+            }
+
+            if(maxPopulation < population){
+                maxPopulation = population;
+                maxYear = currYear;
             }
         }
-        return bestYearToLive;
+        return maxYear;
     }
 };
