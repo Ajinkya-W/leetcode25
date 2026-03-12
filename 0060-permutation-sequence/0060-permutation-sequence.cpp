f23e1f81a783d3d7a9a46f14697dfec03d859b97
Ajinkya Wasnik <47898069+Ajinkya-W@users.noreply.github.com>
2026-03-12 22:03:19 +0530

    Time: 0 ms (100%), Space: 8.8 MB (55.14%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
-        vector<int> nums(n);
-        int counter = 1;
-        for (int i = 0; i < n; i++) {
-            nums[i] = counter++;
+
+        vector<int> nums;
+        vector<int> fact(n);
+        string ans = "";
+
+        // 1. build numbers [1..n]
+        for (int i = 1; i <= n; i++)
+            nums.push_back(i);
+
+        // 2. compute factorials
+        fact[0] = 1;
+        for (int i = 1; i < n; i++)
+            fact[i] = fact[i - 1] * i;
+
+        k--; // convert to 0-index
+
+        // 7. repeat until all digits chosen
+        for (int i = n; i >= 1; i--) {
+            // 4. index = k / (remaining-1)!
+            int idx = k / fact[i - 1];
+
+            // 5. append numbers[index] and remove it
+            ans += to_string(nums[idx]);
+
+            nums.erase(nums.begin() + idx);
+
+            // 6. update k = k % (remaining-1)!
+            k %= fact[i - 1];
         }
 
-        while (k > 1) {
-            k--;
-            nextPermutation(nums);
-        }
-        string kthPermutation = "";
-        for (int num : nums) {
-            kthPermutation += to_string(num);
-        }
-        return kthPermutation;
-    }
-    void nextPermutation(vector<int>& nums) {
-
-        int n = nums.size(), i = n - 2;
-        while (i >= 0 && nums[i] >= nums[i + 1])
-            i--;
-
-        if (i >= 0) {
-            int j = n - 1;
-            while (nums[j] <= nums[i])
-                j--;
-
-            swap(nums[i], nums[j]);
-        }
-
-        reverse(nums.begin() + i + 1, nums.end());
+        return ans;
     }
 };
