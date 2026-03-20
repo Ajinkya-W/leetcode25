1f3d3e5482d4aca9576da00a83b8dc61c2d55a05
Ajinkya Wasnik <47898069+Ajinkya-W@users.noreply.github.com>
2026-03-20 14:36:13 +0530

    Time: 131 ms (9.17%), Space: 191.2 MB (5.1%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,62 +1,42 @@
-struct Node {
-    int key;
-    int val;
-    Node *next;
-    Node *prev;
-    Node(int key, int val) : key(key), val(val), next(nullptr), prev(nullptr) {}
-};
-
 class LRUCache {
 public:
     int capacity;
-    unordered_map<int, Node *> dic;
-    Node *head = new Node(-1, -1);
-    Node *tail = new Node(-1, -1);
-    LRUCache(int capacity) {
-        this->capacity = capacity;
-        head->next = tail;
-        tail->prev = head;
-    }
+    unordered_map<int, list<pair<int, int>>::iterator> itrMap;
+    list<pair<int, int>> lru;
+
+    LRUCache(int capacity) { this->capacity = capacity; }
 
     int get(int key) {
-        if (dic.find(key) == dic.end()) {
-            return -1;
-        }
+        auto it = itrMap.find(key);
 
-        Node *node = dic[key];
-        remove(node);
-        add(node);
-        return node->val;
+        if (it == itrMap.end())
+            return -1;
+
+        int value = it->second->second;
+        lru.erase(it->second);
+        lru.push_front({key, value});
+
+        itrMap.erase(it);
+        itrMap[key] = lru.begin();
+        return value;
     }
 
     void put(int key, int value) {
-        if (dic.find(key) != dic.end()) {
-            Node *oldNode = dic[key];
-            remove(oldNode);
+        auto it = itrMap.find(key);
+
+        if (itrMap.find(key) != itrMap.end()) {
+            lru.erase(it->second);
+            itrMap.erase(it);
         }
 
-        Node *node = new Node(key, value);
-        dic[key] = node;
-        add(node);
+        lru.push_front({key, value});
+        itrMap[key] = lru.begin();
 
-        if (dic.size() > capacity) {
-            Node *nodeToDelete = head->next;
-            remove(nodeToDelete);
-            dic.erase(nodeToDelete->key);
+        if (itrMap.size() > capacity) {
+            auto it = itrMap.find(lru.rbegin()->first);
+            itrMap.erase(it);
+            lru.pop_back();
         }
-    }
-
-    void add(Node *node) {
-        Node *previousEnd = tail->prev;
-        previousEnd->next = node;
-        node->prev = previousEnd;
-        node->next = tail;
-        tail->prev = node;
-    }
-
-    void remove(Node *node) {
-        node->prev->next = node->next;
-        node->next->prev = node->prev;
     }
 };
 
